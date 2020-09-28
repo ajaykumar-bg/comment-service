4812d395713f66260dcf7536f74ecf1f24e3ebf1
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2020-09-28 14:09:51 +0530

    Added dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import rootReducer from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={createStoreWithMiddleware(rootReducer)}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
